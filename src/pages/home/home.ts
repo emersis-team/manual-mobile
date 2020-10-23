a98915e5f4ff7771aac8340aa3d685efcb176bfd
Matias Luzuriaga <mluzuriaga@abstractsolutions.com.ar>
2020-10-23 14:27:44 -0300

    Avances varios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ProteccionPage } from '../proteccion/proteccion';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToProcedimientosCombate() {
+    this.navCtrl.setRoot(ProteccionPage);
   }
 
 }
--- src/pages/proteccion/proteccion.ts
+++ src/pages/proteccion/proteccion.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
 
 @Component({
     selector: 'page-list',
@@ -6,6 +9,86 @@ import { Component } from '@angular/core';
 })
 export class ProteccionPage {
 
-    
+    @ViewChild('slider') slider: Slides;
+    @ViewChild("segments") segments;
+    page: any;
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    goToHome() {
+        this.navCtrl.setRoot(HomePage);
+    }
+
+    // Initialize slider
+    ionViewDidEnter() {
+        this.slideChanged();
+    }
+
+    // On segment click
+    selectedTab(index) {
+        this.slider.slideTo(index);
+        console.log("selectedTab", index)
+    }
+
+    // On slide changed
+    slideChanged() {
+        let currentIndex = this.slider.getActiveIndex();
+        let slides_count = this.segments.nativeElement.childElementCount;
+
+        this.page = currentIndex.toString();
+        if (this.page >= slides_count)
+            this.page = (slides_count - 1).toString();
+
+        console.log("slides_count", slides_count)
+        console.log("this.page", this.page)
+        this.centerScroll();
+    }
+
+    // Center current scroll
+    centerScroll() {
+        if (!this.segments || !this.segments.nativeElement)
+            return;
+
+        let sizeLeft = this.sizeLeft();
+        let sizeCurrent = this.segments.nativeElement.children[this.page].clientWidth;
+        let result = sizeLeft - (window.innerWidth / 2) + (sizeCurrent / 2);
+
+        result = (result > 0) ? result : 0;
+        this.smoothScrollTo(result);
+    }
+
+    // Get size start to current
+    sizeLeft() {
+        let size = 0;
+        for (let i = 0; i < this.page; i++) {
+            size += this.segments.nativeElement.children[i].clientWidth;
+        }
+        return size;
+    }
+
+    // Easing function
+    easeInOutQuart(time, from, distance, duration) {
+        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    }
+
+    // Animate scroll
+    smoothScrollTo(endX) {
+        let startTime = new Date().getTime();
+        let startX = this.segments.nativeElement.scrollLeft;
+        let distanceX = endX - startX;
+        let duration = 400;
+
+        let timer = setInterval(() => {
+            var time = new Date().getTime() - startTime;
+            var newX = this.easeInOutQuart(time, startX, distanceX, duration);
+            if (time >= duration) {
+                clearInterval(timer);
+            }
+            this.segments.nativeElement.scrollLeft = newX;
+        }, 1000 / 60); // 60 fps
+    }
 
 }
